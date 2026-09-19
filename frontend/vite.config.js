277735896a28732b97fad9353c39e63eb1626549
Adwait Patel <145041038+AdwaitPatel@users.noreply.github.com>
2026-09-19 11:37:29 +0530

    Updated Vite Config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
+    '/api':"https://chotu-ai.onrender.com",
       '/api': 'http://localhost:8000',
       '/health': 'http://localhost:8000',
     },
